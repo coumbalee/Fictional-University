2c1441cce40fd7b54b6b70678269da8d3283cd01
coumbalee <coumba.ly@yahoo.com>
2022-05-04 04:09:32 +0200

    custom query for pagination (more than 10 posts)

--- page-past-events.php
+++ page-past-events.php
@@ -13,8 +13,27 @@ get_header(); ?>
 
 <div class="container container--narrow page-section">
   <?php 
-  while(have_posts()){
-    the_post();?>
+
+  
+$today = date('Ymd');
+          $pastEvents = new WP_QUERY(array(
+          'paged'=> get_query_var('paged',1),
+          'post_type'=>'event',
+          'meta_key'=> 'event_date',
+          'orderby'=> 'meta_value_num',
+          'order'=>'ASC',
+          'meta_query'=> array(
+            array(
+             'key'=> 'event_date',
+             'compare'=> '<',
+             'value'=> $today,
+             'type'=> 'numeric'
+            )
+          )
+          ));
+
+  while($pastEvents->have_posts()){
+    $pastEvents->the_post();?>
   <div class="event-summary">
   <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month"><?php 
@@ -29,7 +48,9 @@ get_header(); ?>
             </div>
           </div>
   <?php }
-echo paginate_links();
+echo paginate_links(array(
+    'total'=> $pastEvents->max_num_pages
+));
   ?>
 </div>
 
