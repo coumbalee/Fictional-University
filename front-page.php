42f200a9e5e2d4cf1a869ef85da4d86b9f99b198
coumbalee <coumba.ly@yahoo.com>
2022-05-04 15:47:33 +0200

    Adding link to find your major button

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
     </div>
 
@@ -13,6 +13,7 @@
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+          
           <?php 
           $today = date('Ymd');
           $homepageEvents = new WP_QUERY(array(
