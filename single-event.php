1353c11c8a5648877cc450434d13aaf493331954
coumbalee <coumba.ly@yahoo.com>
2022-05-04 14:55:49 +0200

    Related program links for each single event

--- single-event.php
+++ single-event.php
@@ -21,6 +21,22 @@ while(have_posts()){
 
     <div class="generic-content">
         <?php the_content();?>
+
+        <?php 
+        $relatedProgram = get_field('related_programs');
+
+        if($relatedProgram){
+          echo'<hr class=" section-break">';
+          echo '<h2 class ="headline headline--mediume">Related Program(s)</h2>';
+          echo '<ul class=" link-list  min-list">';
+          foreach($relatedProgram as $program){?>
+            
+          <li><a href ="<?php echo get_the_permalink($program); ?> "><?php echo get_the_title($program); ?></a></li>
+  
+          <?php }
+          echo '</ul>';
+        }
+        ?>
     </div>
     </div>
     <?php
