0d6bc81d386b8e06e5dc03a691e2d635cefab45f
coumbalee <coumba.ly@yahoo.com>
2022-05-06 13:43:17 +0200

    condition to set default subtitle if there is no

--- functions.php
+++ functions.php
@@ -5,13 +5,17 @@ function page_banner($args){
      $args['title'] = get_the_title();
     }
 
+    if(!$args['subtitle']){
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
 ?>
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
         <div class="page-banner__intro">
-          <p><?php  the_field('page_banner_subtitle');?></p>
+          <p><?php  echo $args['subtitle'];?></p>
         </div>
       </div>
     </div>
